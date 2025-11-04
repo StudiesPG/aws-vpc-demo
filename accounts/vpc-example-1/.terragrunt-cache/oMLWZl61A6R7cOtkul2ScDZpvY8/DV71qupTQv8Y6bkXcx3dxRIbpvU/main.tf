dc15b23c9706193cd0d939ccf9524843a451500b
Guilherme-Goldmann <guilhermegarguedes@gmail.com>
2025-11-04 12:48:28 -0300

    rm terragrunt-cache dir

--- accounts/vpc-example-1/.terragrunt-cache/oMLWZl61A6R7cOtkul2ScDZpvY8/DV71qupTQv8Y6bkXcx3dxRIbpvU/main.tf
+++
@@ -1,7 +0,0 @@
-# Main file of the module, defining the resource(s) to be provisioned
-
-resource "aws_vpc" "main-demo" {
-  cidr_block =  var.cidr_block
-  region = var.region
-  tags = var.tags
-}
--- accounts/vpc-example-1/.terragrunt-cache/oMLWZl61A6R7cOtkul2ScDZpvY8/DV71qupTQv8Y6bkXcx3dxRIbpvU/version.tf
+++
@@ -1,5 +0,0 @@
-# File to expose which terraform version is required to run this module.
-
-terraform {
-  required_version = ">=0.12" 
-}
